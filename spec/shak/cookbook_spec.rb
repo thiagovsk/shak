8300a4231dbdf54755e8b66c53f1b16362a216fd
Thiago Ribeiro <thiagitosouza@hotmail.com>
2015-05-08 17:54:29 -0300

    Fix deprecation warning for rspec stubs.

--- spec/shak/cookbook_spec.rb
+++ spec/shak/cookbook_spec.rb
@@ -7,8 +7,7 @@ describe Shak::Cookbook do
   it 'lists all cookbooks' do
     FileUtils.mkdir_p(@tmpdir + '/foo')
     FileUtils.mkdir_p(@tmpdir + '/bar')
-    Shak.config.stub(:cookbooks_dir).and_return(@tmpdir)
-
+    allow(Shak.config).to receive(:cookbooks_dir).and_return(@tmpdir)
     cookbooks = Shak::Cookbook.all
     expect(cookbooks.map(&:name).sort.reverse).to eq(['foo', 'bar'])
   end
